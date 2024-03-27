7e5eb83f3aec1f508628b8e23a89e8d3c4555773
Neri Gariboglio <82184840+NeriGariboglio@users.noreply.github.com>
2024-03-27 15:55:16 -0300

    configuracion sensor de tension

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,28 @@
 
 EnergyMonitor sensors;                  
 
+float voltageRMS;
 float currentRMS;
 void setup()
 {  
   Serial.begin(9600);
   analogReadResolution(12);
   sensors.current(A6, 7.85);
+  sensors.voltage(A7,185,1.7);//con alimentacion externa de 3.25v
 }
 
 void loop()
 {
   sensors.calcVI(20,2000);
-  currentRMS= sensors.Irms;
-  Serial.println(currentRMS);
+  //voltageRMS= sensors.Vrms;
+  //currentRMS= sensors.Irms;
+  //Serial.println(voltageRMS);
+  //Serial.println(currentRMS);
+    sensors.serialprint();           // Print out all variables (realpower, apparent power, Vrms, Irms, power factor)
+  
+  float realPower       = sensors.realPower;        //extract Real Power into variable
+  float apparentPower   = sensors.apparentPower;    //extract Apparent Power into variable
+  float powerFActor     = sensors.powerFactor;      //extract Power Factor into Variable
+  float supplyVoltage   = sensors.Vrms;             //extract Vrms into Variable
+  float Irms            = sensors.Irms;             //extract Irms into Variable
 }
