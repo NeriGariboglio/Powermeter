0cab1dbb81943dc52f7539d7260f9f136d6385bb
Neri Gariboglio <82184840+NeriGariboglio@users.noreply.github.com>
2024-03-27 22:34:50 -0300

    Sincronizar RTC-NTP

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,28 @@
 #include <Arduino.h>
+#include <ESP32Time.h>
 #include <WiFi.h>
 #include "FirebaseESP32.h"
 #include "EmonLib.h"                 
+
 #define FIREBASE_HOST "powermeter-esp32-default-rtdb.firebaseio.com"
 #define FIREBASE_AUTH "LuEhdnkBM7XuM2AaGiyUHXLNxeew51TucH8Lifeo"
+#define emonTxV3    1
+
+ESP32Time rtc;
+FirebaseData firebaseData;
+EnergyMonitor sensors;                  
+String nodo="/Sensores";
+
+//configuracion servidor NPT
+const char* ntpServer="ntp.lonelybinary.com";
+const long gmtOffset_sec=-3*3600;// GMT -3 horas
+const int daylightOffset_sec=0;// Ajuste por horario de verano de 1 hora (3600 segundos)
+
 
 const char* ssid="Fibertel WiFi764 2.4GHz";
 const char* password="00444074551";
 
-#define emonTxV3    1
-
-FirebaseData firebaseData;
-EnergyMonitor sensors;                  
-
-String nodo="/Sensores";
 bool iterar=true;
-
 float currentRMS=0;
 void setup()
 {  
@@ -33,19 +40,27 @@ void setup()
   Serial.print("IP asignada ");
   Serial.print(WiFi.localIP());
 
+  
+
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
   Firebase.reconnectWiFi(true);
 
   analogReadResolution(12);
   sensors.current(A6, 7.85);
   delay(1000);
+  //ya conectado a internet se actualiza el rtc interno
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  struct tm timeinfo;
+  if (getLocalTime(&timeinfo)){
+  rtc.setTimeStruct(timeinfo);  
+  }
 }
 
 void loop()
 {
-  sensors.calcVI(20,2000);
+  //sensors.calcVI(20,2000);
   currentRMS= sensors.Irms;
-  //Serial.println(currentRMS);
+  Serial.println(currentRMS);
   Firebase.setInt(firebaseData, nodo +"/corriente",currentRMS);
-  
+  Serial.println(rtc.getTime("%d/%m/%Y %H:%M:%S"));
 }
